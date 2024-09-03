6722b3d081ac24386495d57e724bb033fcc2f39a
Jorge Martins <jorgelucas.rm@outlook.com>
2024-09-03 13:17:09 -0300

    [Backend]Feat:Primeiro commit com a base do projeto (#1)

---
+++ backend/src/main/java/senac/ads/projeto_integrador_iv/Main.java
@@ -0,0 +1,13 @@
+package senac.ads.projeto_integrador_iv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
