d654f631b53d1439a0aba8024e98b3664bc1aaba
BomDiaSol <lul.rar@outlook.com>
2024-09-07 21:10:09 -0300

    [Backend]Feat: Categoria e Marca controller e Service

---
+++ backend/src/main/java/senac/ads/projeto_integrador_iv/controller/CategoriaController.java
@@ -0,0 +1,47 @@
+package senac.ads.projeto_integrador_iv.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import senac.ads.projeto_integrador_iv.models.Categoria;
+import senac.ads.projeto_integrador_iv.service.CategoriaService;
+
+@RestController
+@RequestMapping("/api/v1/categoria")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> buscarTodos(){
+        return categoriaService.buscarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarCategoriaPorId(@PathVariable UUID id){
+        return categoriaService.buscarCategoriaPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity criarCategoria(@RequestBody Categoria categoria){
+        return categoriaService.salvarCategoria(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity atualizarCategoria(@PathVariable("id") UUID id, @RequestBody Categoria categoria){
+        return categoriaService.atualizarCategoria(id, categoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarCategoria(@PathVariable UUID id){
+        categoriaService.deletarCategoria(id);
+        return ResponseEntity.noContent().build();
+    }
+}
