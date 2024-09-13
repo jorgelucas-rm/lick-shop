918cd94966b6e23f41c89386e8b5ba30d6cc835f
Jorge Martins <jorgelucas.rm@outlook.com>
2024-09-12 21:51:43 -0300

    [Backend]Fix:Corrigindo a logica dos serviços de marca e categoria

---
+++ backend/src/main/java/senac/ads/projeto_integrador_iv/dto/MarcaTO.java
@@ -0,0 +1,10 @@
+package senac.ads.projeto_integrador_iv.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MarcaTO {
+    private String nome;
+}
