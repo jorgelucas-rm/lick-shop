5ee0b12f9539b81b67a9f556ee231a21bf2c445b
Jorge Lucas <jorgelucas.rm@outlook.com>
2024-11-14 15:29:55 -0300

    [Backend]Feat:Adicionando o método https à requisição do swagger

--- backend/src/main/java/senac/ads/projeto_integrador_iv/infra/swagger/OpenApiConfig.java
+++ backend/src/main/java/senac/ads/projeto_integrador_iv/infra/swagger/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,6 +19,7 @@ public class OpenApiConfig {
                 .info(new Info().title("API Documentation")
                         .version("1.0")
                         .description("API documentation using OpenAPI 3"))
+                .addServersItem(new Server().url("https://api.lickshop.acilab.com.br"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("bearerAuth",
